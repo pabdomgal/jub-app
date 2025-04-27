cb10ba402d52250447d6f56979bf10c2f7906426
Pablo Domínguez <92788663+pabdomgal@users.noreply.github.com>
2025-04-27 14:16:38 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "jub-app",
+  "version": "1.0.0",
+  "description": "API de Jubilación",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.1"
+  },
+  "engines": {
+    "node": "14.x"
+  }
+}
